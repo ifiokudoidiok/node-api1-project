fdfde9abfed45be4aed01e35f19f84e7eb53b97c
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-28 13:42:12 +0100

    Dependencies installed; server running

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+const express = require("express");
+const cors = require("cors");
+
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+
+server.get('*', handleRequest)
+
+function handleRequest(req, res) {
+    res.json('hellos ' + req)
+}
+
+
+server.listen(process.env.PORT || 5000, () => {
+    console.log('running on ', process.env.PORT || 5000)
+})
+ 
